3899b57d25fa5b1c5b81b6b9cda128076a2a9992
Ana <ana@iwmnetwork.com>
2020-06-18 21:19:07 +0200

    Add Experience section

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,70 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const imagemin = require("gulp-imagemin");
-const concat = require("gulp-concat");
-const terser = require("gulp-terser");
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require("gulp");
+// Importing all the Gulp-related packages we want to use
 const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+var replace = require("gulp-replace");
 const browserSync = require("browser-sync").create();
-const { src, series, parallel, dest } = require("gulp");
-const jsPath = "src/assets/js/**/*.js";
-const stylePath = "src/assets/scss/**/*.scss";
 
-function copyHtml() {
-  return src("src/*.html").pipe(gulp.dest("dist"));
+// File paths
+const files = {
+  scssPath: "src/assets/scss/**/*.scss",
+  jsPath: "src/assets/js/**/*.js",
+};
+
+// Sass task: compiles the style.scss file into style.css
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass()) // compile SCSS to CSS
+    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+    .pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
+    .pipe(dest("dist/css")); // put final CSS in dist folder
 }
-function imgTask() {
-  return src("src/assets/images/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("dist/images"));
-}
+
+// JS task: concatenates and uglifies JS files to script.js
 function jsTask() {
-  return src(jsPath)
-    .pipe(sourcemaps.init())
+  return src([
+    files.jsPath,
+    //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+  ])
     .pipe(concat("all.js"))
-    .pipe(terser())
-    .pipe(sourcemaps.write("."))
-    .pipe(dest("dist/js/"));
+    .pipe(uglify())
+    .pipe(dest("dist/js"));
 }
 
-// //compile scss into css
-function styleTask() {
-  return gulp
-    .src(stylePath)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./dist/css/"))
-    .pipe(browserSync.stream());
+// Cachebust
+function cacheBustTask() {
+  var cbString = new Date().getTime();
+  return src(["index.html"])
+    .pipe(replace(/cb=\d+/g, "cb=" + cbString))
+    .pipe(dest("."));
 }
 
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
 function watchTask() {
   browserSync.init({
     server: {
-      baseDir: "./dist",
-      index: "/index.html",
+      baseDir: "./",
+      index: "index.html",
     },
   });
-  gulp.watch(stylePath, styleTask);
-  gulp.watch("./src/*.html").on("change", browserSync.reload);
-  gulp.watch(jsPath).on("change", browserSync.reload);
+  watch(
+    [files.scssPath, files.jsPath],
+    { interval: 1000, usePolling: true }, //Makes docker work
+    series(parallel(scssTask, jsTask), cacheBustTask)
+  );
+  watch("./index.html").on("change", browserSync.reload);
 }
 
-exports.copyHtml = copyHtml;
-exports.imgTask = imgTask;
-exports.jsTask = jsTask;
-exports.styleTask = styleTask;
-exports.watchTask = watchTask;
-exports.default = parallel(copyHtml, imgTask, jsTask, styleTask, watchTask);
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
