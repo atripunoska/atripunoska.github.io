c6a7a04c089e0ebdbd9fb5d69b8b5b38fb650351
atripunoska <atripunoska@gmail.com>
2023-09-14 15:29:36 +0200

    feat: add navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./sections/Navbar";
+import DotGroup from "./sections/DotGroup";
 function App() {
-  return <div className="App"></div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
