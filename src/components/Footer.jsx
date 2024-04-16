f763541dcfb98b0e84ea55e57aa87b411fe565c3
atripunoska <atripunoska@gmail.com>
2024-04-16 14:38:49 +0200

    feat: add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      id="footer"
+      className="bg-deep-blue px-16 py-5 text-xs font-light text-center text-white"
+    >
+      © {new Date().getFullYear()} Ana Tripunoska
+    </footer>
+  );
+};
+
+export default Footer;
