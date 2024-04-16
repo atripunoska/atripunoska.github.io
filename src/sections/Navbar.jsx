1cb89339716c59195b0125b79717d6ce2a9bb7fc
atripunoska <atripunoska@gmail.com>
2024-04-16 14:39:14 +0200

    feat: update active color on selected page

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -8,8 +8,8 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
     <AnchorLink
       className={`${
         selectedPage === lowerCasePage
-          ? "text-black dark:text-gray-200"
-          : "dark-grey dark:text-white"
+          ? "text-dark-purple dark:text-light-green"
+          : "text-dark-grey dark:text-white"
       } hover:text-beige transition duration-500 font-mono font-light`}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
@@ -32,12 +32,12 @@ const Navbar = ({
 
   const navbarBackground = isTopOfPage
     ? ""
-    : "bg-white dark:bg-deep-blue drop-shadow-md dark:drop-shadow-[0_4px_3px_rgba(180,180,180,0.25)]";
+    : "bg-white dark:bg-deep-blue/80 md:drop-shadow-md  dark:backdrop-blur-md";
   return (
-    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-4`}>
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-4 isolate`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold text-black dark:text-white">
-          ATZ
+          A
         </h4>
 
         {isAboveSmallScreens ? (
@@ -69,7 +69,7 @@ const Navbar = ({
             />
             <button
               onClick={handleDarkMode}
-              class="h-12 w-12 rounded-lg p-2 dark:bg-gray-800 bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 z-50 relative"
+              className="h-12 w-12 rounded-lg p-2 dark:bg-gray-800 bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 z-50 relative"
             >
               {!isDarkMode ? (
                 <svg fill="gray-400" viewBox="0 0 20 20">
@@ -79,30 +79,74 @@ const Navbar = ({
                 <svg fill="#fff" viewBox="0 0 20 20">
                   <path
                     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               )}
             </button>
           </div>
         ) : (
-          <button
-            className="rounded-full bg-red p-2"
-            onClick={() => setIsMenuToggled(!isMenuToggled)}
-          >
-            <img src="../assets/menu-icon.svg" alt="Menu" />
-          </button>
+          <div>
+            <button
+              onClick={handleDarkMode}
+              className="h-12 w-12 rounded-lg p-2 dark:bg-gray-800 bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 z-50 relative"
+            >
+              {!isDarkMode ? (
+                <svg fill="gray-400" viewBox="0 0 20 20">
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              ) : (
+                <svg fill="#fff" viewBox="0 0 20 20">
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              )}
+            </button>
+            <button
+              className="rounded-full dark:bg-white bg-deep-blue p-2"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+            >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z"
+                  className="fill-white dark:fill-black"
+                />
+              </svg>
+            </button>
+          </div>
         )}
         {!isAboveSmallScreens && isMenuToggled && (
-          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+          <div className="fixed right-0 bottom-0 h-full dark:bg-deep-blue bg-white w-[300px]">
             <div className="flex justify-end p-12 ">
               <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                <img src="../assets/close-icon.svg" alt="Close" />
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                    className="fill-black dark:fill-white"
+                    fill-opacity="0.54"
+                  />
+                </svg>
               </button>
             </div>
 
-            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl dark:text-white text-deep-blue">
               <Link
                 page="Home"
                 selectedPage={selectedPage}
