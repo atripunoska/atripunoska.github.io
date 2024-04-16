67f09d26fce4ea9b00579eaebbb5389919331065
atripunoska <atripunoska@gmail.com>
2024-04-16 14:46:06 +0200

    refactor: update attribute name for svg

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -117,8 +117,8 @@ const Navbar = ({
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z"
                   className="fill-white dark:fill-black"
                 />
