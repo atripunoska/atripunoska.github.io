e10391f138c029895ef0cb2eb127ae3f67944e93
atripunoska <atripunoska@gmail.com>
2026-06-19 17:09:27 +0200

    feat: port design tokens, bridge into tailwind v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className='text-red-600'>hello</div>;
+  return (
+    <div className='min-h-screen bg-bg text-fg flex items-center justify-center font-sans'>
+      <h1 className='text-4xl font-serif italic'>hello</h1>
+    </div>
+  );
 }
 
 export default App;
